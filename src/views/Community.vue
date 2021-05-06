e96b98caffe4121f6ebb10bde2edc7a15c6224a5
weichenhao <weichenhao@qutoutiao.net>
2021-05-06 18:59:45 +0800

    社区页面正在开发

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="community">
     <Header></Header>
-    <a>community</a>
+    <el-tabs v-model="activeName" >
+      <el-tab-pane label="同城交流" name="first">
+        <el-row :gutter="5">
+          <el-col :span="12"  v-for="(model,key) in models" :key="key" style="margin-top: 10px">
+            <span @click="gameDetail(model)">
+              <el-card :body-style="{ padding: '0px' }" shadow="hover" >
+                <img :src="model.url" class="image">
+                <div style="padding: 10px;">
+                  <span>{{ model.name }}</span>
+                  <span style="margin-left: 10px">{{model.price}}</span>
+                </div>
+              </el-card>
+            </span>
+
+          </el-col>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="同车分享" name="second">同车分享</el-tab-pane>
+      <el-tab-pane label="老司机贴" name="thrid">老司机贴</el-tab-pane>
+    </el-tabs>
     <Footer></Footer>
   </div>
 </template>
@@ -15,7 +34,22 @@ export default {
     Header,
     Footer
   },
-  name: 'Community'
+  name: 'Community',
+  data(){
+    return {
+      ruleForm: {
+
+      },
+      activeName: 'first',
+    }
+  },
+  mounted:function () {
+
+  },
+  methods: {
+
+  }
+
 }
 </script>
 
